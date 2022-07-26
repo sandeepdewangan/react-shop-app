adaccd96f5cc7c1eb9b9b25522637f893a994e40
sandeepdewangan <sandeepdewangan7@gmail.com>
2022-07-26 20:29:31 +0530

    Meals page header

---
+++ src/components/Layout/Headers.js
@@ -0,0 +1,14 @@
+const { Fragment } = require("react");
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header>
+        <h2>The Meals App</h2>
+        <button>Cart</button>
+      </header>
+    </Fragment>
+  );
+};
+
+export default Header;
